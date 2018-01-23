1bce7cf2d917fa607051392172094bb39e592b49
91DarioDev <dariomsn@hotmail.it>
2018-01-23 15:25:27 +0100

    github

--- cryptotrackerbot/utils.py
+++ cryptotrackerbot/utils.py
@@ -13,3 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with CryptoTrackerBot.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def send_autodestruction_message(bot, update):
+	
